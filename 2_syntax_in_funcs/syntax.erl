8686235e09eb59367e93dc9191d2385c0a39e571
Avi Fox-Rosen <avi.foxrosen@imagineeasy.com>
2016-02-01 09:59:04 -0500

    init commit learning erlang

---
+++ 2_syntax_in_funcs/syntax.erl
@@ -0,0 +1,8 @@
+-module(syntax).
+
+greet(male, Name) ->
+	io:format("Hello, Mr. ~s!", [Name]);
+greet(female, Name) ->
+	io:format("Hello, Mrs. ~s!", [Name]);
+greet(_, Name) ->
+	io:format("Hello, ~s!", [Name]).
